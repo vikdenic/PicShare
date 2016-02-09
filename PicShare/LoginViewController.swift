6e1eb747124242170dcf3d566ab2689378d5ccaa
vik denic <vik.denic@gmail.com>
2016-02-09 10:41:59 -0600

    set up login vc

---
+++ PicShare/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  PicShare
+//
+//  Created by Vik Denic on 2/9/16.
+//  Copyright © 2016 nektar labs. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func onSignUpButtonTapped(sender: AnyObject) {
+
+    }
+
+    @IBAction func onLoginButtonTapped(sender: AnyObject) {
+
+    }
+}
